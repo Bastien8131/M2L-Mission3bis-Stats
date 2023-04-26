cd117e1fd90e5a04cd43c6b5b2159c2057a8839c
Ast415 <bastienbories81@gmail.com>
2023-04-26 18:46:11 +0200

    id

--- app/src/main/java/com/btssio/m2l_mission3bis_stats/FormationDomaineData.java
+++ app/src/main/java/com/btssio/m2l_mission3bis_stats/FormationDomaineData.java
@@ -1,12 +1,24 @@
 package com.btssio.m2l_mission3bis_stats;
 
 public class FormationDomaineData {
+    private int domainId;
+    private int formationId;
     private String domainLabel;
     private String formationNom;
 
-    public FormationDomaineData(String domainLabel, String formationNom) {
+    public FormationDomaineData(String domainLabel, String formationNom, int domainId, int formationId) {
         this.domainLabel = domainLabel;
         this.formationNom = formationNom;
+        this.domainId = domainId;
+        this.formationId = formationId;
+    }
+
+    public int getDomainId() {
+        return domainId;
+    }
+
+    public int getFormationId() {
+        return formationId;
     }
 
     public String getDomainLabel() {
@@ -17,3 +29,4 @@ public class FormationDomaineData {
         return formationNom;
     }
 }
+
